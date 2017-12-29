5b6442363668d4b48b069776ea29f0d609b274c2
Raymundo Manuel Alvarez <rollingdash68@gmail.com>
2017-12-29 06:14:17 -0800

    Add files via upload

---
+++ truncateAString.js
@@ -0,0 +1,10 @@
+unction truncateString(str, num) {
+  // Clear out that junk in your trunk
+  
+    var result = (str.length <= 3 || num <= 3) ? str.slice(0 , num) + "..." :
+    (str.length > num) ? str.slice(0,num -3)+ "..." :  str ;
+  	return result;
+    
+}
+
+truncateString("A-tisket a-tasket A green and yellow basket", 11);
