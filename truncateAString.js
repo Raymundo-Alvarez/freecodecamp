4d0ecdb7b342b6391c4f435037b14c669dc8844b
Raymundo Manuel Alvarez <rollingdash68@gmail.com>
2017-12-31 15:27:00 -0800

    Update truncateAString.js

--- truncateAString.js
+++ truncateAString.js
@@ -1,8 +1,7 @@
 function truncateString(str, num) {
   // Clear out that junk in your trunk
   
-    var result = (str.length <= 3 || num <= 3) ? str.slice(0 , num) + "..." :
-    (str.length > num) ? str.slice(0,num -3)+ "..." :  str ;
+    var result = (str.length <= 3 || num <= 3) ? str.slice(0 , num) + "..." : (str.length > num) ? str.slice(0,num -3)+ "..." :  str ;
   	return result;
     
 }
